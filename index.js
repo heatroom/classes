647643989e2a68396ce8802f6038c670274fa1f6
foriacus <634844533@qq.com>
2014-02-26 16:09:13 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+var classes = require('classes');
+
+
+/**
+ * Expose `mixin`.
+ */
+
+module.exports = exports = mixin;
+
+
+/**
+ * Mixin the classes methods.
+ *
+ * @param {Object} object
+ * @return {Object}
+ */
+
+function mixin (obj) {
+  for (var method in exports) obj[method] = exports[method];
+  return obj;
+}
+
+
+/**
+ * Add a class.
+ *
+ * @param {String} name
+ * @return {Object}
+ */
+
+exports.addClass = function (name) {
+  classes.add(name, this.el[0]);
+  return this;
+};
+
+
+/**
+ * Remove a class.
+ *
+ * @param {String} name
+ * @return {Object}
+ */
+
+exports.removeClass = function (name) {
+  classes.remove(name, this.el[0]);
+  return this;
+};
+
+
+/**
+ * Has a class?
+ *
+ * @param {String} name
+ * @return {Boolean}
+ */
+
+exports.hasClass = function (name) {
+  return classes.has(name, this.el[0]);
+};
+
+
+/**
+ * Toggle a class.
+ *
+ * @param {String} name
+ * @return {Object}
+ */
+
+exports.toggleClass = function (name) {
+  classes.toggle(name, this.el[0]);
+  return this;
+};
